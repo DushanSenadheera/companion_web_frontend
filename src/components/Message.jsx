12c38cb31ad4d08f215403db5c7363cbe8af8252
Dushan Senadheera <dcsenadheera777@gmail.com>
2023-04-18 21:19:25 +0530

    added nav section content

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,93 @@
-export default function Message(){
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import TextField from '@mui/material/TextField';
+import SendIcon from '@mui/icons-material/Send';
+import '../styles/message.css';
+
+export default function Message() {
+
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
     return (
-        <div>
-            <h3>Message</h3>
+        <div className='msgContent'>
+            <h3>Messages</h3>
+            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel3bh-content"
+                    id="panel3bh-header"
+                >
+                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                        Advanced settings
+                    </Typography>
+                    <Typography sx={{ color: 'text.primary' }}>
+                        Filtering has been entirely disabled for whole web server
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={4}
+                        fullWidth={true}
+                    />
+                </AccordionDetails>
+            </Accordion>
+            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel3bh-content"
+                    id="panel3bh-header"
+                >
+                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                        Advanced settings
+                    </Typography>
+                    <Typography sx={{ color: 'text.primary' }}>
+                        Filtering has been entirely disabled for whole web server
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={4}
+                        fullWidth={true}
+                    />
+                </AccordionDetails>
+            </Accordion>
+            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel3bh-content"
+                    id="panel3bh-header"
+                >
+                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                        Advanced settings
+                    </Typography>
+                    <Typography sx={{ color: 'text.primary' }}>
+                        Filtering has been entirely disabled for whole web server
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={4}
+                        fullWidth={true}
+                    />
+                </AccordionDetails>
+            </Accordion>
         </div>
     )
 }
