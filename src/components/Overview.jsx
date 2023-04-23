9038059867da46ee1552285c9c1a8baf04ebbf2a
Dushan Senadheera <dcsenadheera777@gmail.com>
2023-04-24 00:02:35 +0530

    made some styling changes

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,7 +1,39 @@
+import '../styles/overview.css';
+import { useState } from 'react';
+import Axios from 'axios';
+
 export default function Overview(){
+
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    function handleFileInput(e) {
+        setSelectedFile(e.target.files[0]);
+    }
+
+    function handleUpload(e) {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append('file', selectedFile);
+
+        Axios.post('http://localhost:3001/upload', formData)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     return (
-        <div>
+        <div className='overviewSection'>
             <h3>Overview</h3>
+            <div className="overviewContent">
+                {/* <form onSubmit={handleUpload}>
+                    <input type="file" onChange={handleFileInput} />
+                    <button type="submit">Upload</button>
+                </form> */}
+            </div>
         </div>
     )
 }
---
+++ src/components/Settings.jsx
@@ -0,0 +1,98 @@
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Button from '@mui/material/Button';
+import '../styles/settings.css';
+import { useState } from 'react';
+
+export default function Settings() {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <div className='pswChangeSection'>
+            <h3>Settings</h3>
+            <div className="pswChange">
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" size='small'>
+                    <InputLabel htmlFor="outlined-adornment-password">Old Password</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Old Password"
+                    />
+                </FormControl>
+                <br />
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" size='small'>
+                    <InputLabel htmlFor="outlined-adornment-password">New Password</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="New Password"
+                    />
+                </FormControl>
+                <br />
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" size='small'>
+                    <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="confirm Password"
+                    />
+                </FormControl>
+                <br />
+                <Button sx={
+                    {
+                        backgroundColor: 'var(--primary-color)',
+                    }
+                } size='small' variant="contained">Submit</Button>
+            </div>
+        </div>
+    )
+}
